c1a707d566f71ee0d251ac93893b482c491f593c
orcunkiral <orcunkiral35@gmail.com>
2024-01-01 13:49:58 -0500

    modified login and sign-up

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -13,22 +13,22 @@ const Login = () => {
             <h2>Login</h2>
           </div>
           <div className="login-text">
-            Are you a new member? <span><a href="../Sign_Up/Sign_Up.html" style="color: #2190FF;"> Sign Up Here</a></span>
-          </div>
+                        Are you a new member? <span><Link to="/signup" style={{ color: '#2190FF' }}> Sign Up Here</Link></span>
+            </div>
           <br />
           <div className="login-form">
             <form>
               <div className="form-group">
-                        <label for="email">Email</label>
+                        <label htmlFor="email">Email</label>
                         <input type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
                     </div>
               <div className="form-group">
-                <label for="password" >Password</label>
+                <label htmlFor="password" >Password</label>
                 <input
                   type="password"
                   nameName="password"
                   id="password"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Enter your password"
                   aria-describedby="helpId"
                 />
--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -7,30 +7,30 @@ const Sign_Up = () => {
     const handleClick = () => setClick(!click);
     const [click, setClick] = useState(false);
     return (
-    <div className="container" style="margin-top: 5%;">
+        <div className="container" style={{ marginTop: '5%' }}>
         <div className="signup-grid">
             <div className="signup-text">
                 <h1>Sign Up</h1>
             </div>
-            <div className="signup-text1" style="text-align: left;">
-                Already a member? <span><a href="../Login/Login.html" style="color: #2190FF;"> Login</a></span>
-            </div>
+            <div className="signup-text1" style={{ textAlign: 'left' }}>
+                    Already a member? <span><Link to="/login" style={{ color: '#2190FF' }}> Login</Link></span>
+                </div>
             <div className="signup-form">
                 <form>
                     <div className="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" required class="form-control" placeholder="Enter your name" aria-describedby="helpId" /> </div>
-                    <div class="form-group">
-                        <label for="phone">Phone</label>
-                        <input type="tel" name="phone" id="phone" required class="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" required className="form-control" placeholder="Enter your name" aria-describedby="helpId" /> </div>
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone</label>
+                        <input type="tel" name="phone" id="phone" required className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
                     </div>
                     <div className="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" required class="form-control" placeholder="Enter your email" aria-describedby="helpId" />
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" required className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
                     </div>
                     <div className="form-group">
-                        <label for="password">Password</label>
-                        <input name="password" id="password" required class="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+                        <label htmlFor="password">Password</label>
+                        <input name="password" id="password" required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
                     </div>
                     <div className="btn-group">
                         <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
