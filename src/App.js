46a8a40334247c0d9f23d193d9a7d199e0eb0d76
orcunkiral <orcunkiral35@gmail.com>
2024-01-05 15:57:55 -0500

    reports

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import BookingConsultation from './Components/BookingConsultation';
 import Notification from './Components/Notification/Notification';
 import ReviewForm from './Components/ReviewForm/ReviewForm';
+import ProfileForm from './Components/ProfileCard/ProfileCard';
+import ReportsLayout from './Components/ReportsLayout/ReportsLayout';
 
 function App() {
     return (
@@ -24,6 +26,8 @@ function App() {
                     <Route path="/finddoctor" element={<FindDoctorSearch />} />
                     <Route path='/search/doctors' element={<BookingConsultation />} />
                     <Route path='/reviews' element={<ReviewForm />} />
+                    <Route path='/profile' element={<ProfileForm />} />
+                    <Route path='/reports' element={<ReportsLayout />} />
                 </Routes>
             </Notification>
         </BrowserRouter>
