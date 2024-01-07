2f647543c0f3efd254bdcb50455b495418347d6d
orcunkiral <orcunkiral35@gmail.com>
2024-01-07 11:22:33 -0500

    deployment solved

--- server/index.js
+++ server/index.js
@@ -4,20 +4,14 @@ const connectToMongo = require('./db');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8181;
-
-
 // Middleware
 app.use(express.json());
 app.use(cors());
-
 // Connect to MongoDB
 connectToMongo();
-
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-
 app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
@@ -25,7 +19,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
   // Start the server
-
 app.listen(PORT, () => {
 console.log(`Server is running on port http://localhost:${PORT}`);
 });
